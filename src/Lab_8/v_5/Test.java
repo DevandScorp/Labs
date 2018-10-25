f11e2bf5cac85a62d8a79b5bb5ee1c04376b343a
DevandScorp <38842788+DevandScorp@users.noreply.github.com>
2018-10-25 10:52:15 +0300

    feat: Lab_8.v_5 solution

--- src/Lab_8/v_5/Test.java
+++ src/Lab_8/v_5/Test.java
@@ -6,6 +6,82 @@ import java.util.*;
 public class Test {
     private static final String PATH = "src\\Lab_8\\v_5\\data.dat";
 
+    public static User move(Object index, Map<Object, Long> users, int direction) {
+        Object[] objects = users.keySet().toArray();
+        if (direction == 0) {
+            for (int i = 0; i < objects.length - 1; ++i) {
+                if (objects[i].equals(index)) {
+                    try (RandomAccessFile data = new RandomAccessFile(PATH, "rw")) {
+                        int j = i + 1;
+                        data.seek(users.get(objects[j]));
+                        byte[] b = new byte[1024];
+                        data.read(b);
+                        User deserialize = deserialize(b);
+                        while (deserialize.isDeleted()) {
+                            ++j;
+                            if (j >= objects.length) return null;
+                            data.seek(users.get(objects[j]));
+                            byte[] bytes = new byte[1024];
+                            data.read(bytes);
+                            deserialize = deserialize(b);
+                        }
+                        return deserialize;
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } else if (direction == 1) {
+            for (int i = 1; i < objects.length; ++i) {
+                if (objects[i].equals(index)) {
+                    try (RandomAccessFile data = new RandomAccessFile(PATH, "rw")) {
+                        int j = i - 1;
+                        data.seek(users.get(objects[j]));
+                        byte[] b = new byte[1024];
+                        data.read(b);
+                        User deserialize = deserialize(b);
+                        while (deserialize.isDeleted()) {
+                            --j;
+                            if (j < 0) return null;
+                            data.seek(users.get(objects[j]));
+                            byte[] bytes = new byte[1024];
+                            data.read(bytes);
+                            deserialize = deserialize(b);
+                        }
+                        return deserialize;
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void removeByIndex(Object index, Map<Object, Long> users) {
+        for (var entry : users.entrySet()) {
+            if (entry.getKey().equals(index)) {
+                try (RandomAccessFile data = new RandomAccessFile(PATH, "rw")) {
+                    data.seek(entry.getValue());
+                    byte[] b = new byte[1024];
+                    data.read(b);
+                    User deserialize = deserialize(b);
+                    deserialize.setDeleted(true);
+                    data.seek(entry.getValue());
+                    data.write(serialize(deserialize));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     public static Map<Object, Long> write(List<User> users) {
         Map<Object, Long> indexList = new HashMap<>();
         try (RandomAccessFile data = new RandomAccessFile(PATH, "rw")) {
@@ -32,6 +108,8 @@ public class Test {
             data.seek(dataPointer);
             byte[] b = new byte[1024];
             data.read(b);
+            User deserialize = deserialize(b);
+            if (deserialize.isDeleted()) return null;
             return deserialize(b);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -99,23 +177,40 @@ public class Test {
                         1432671,
                         new Date(10000))
         );
-        User.setField("lastPay");
+        User.setField("userPay");
         Map<Object, Long> write = write(users);
+
         System.out.println("Вывод данных по убыванию ключа");
         for (var entry : write.entrySet()) {
             System.out.println("Key: " + entry.getKey());
             System.out.println(read(entry.getKey(), write));
         }
+
         System.out.println();
         System.out.println("Вывод данных по возрастанию ключа");
         Object[] entries = write.keySet().toArray();
         Arrays.sort(entries);
-
         Arrays.stream(entries).forEach(a->{
             System.out.println("Key: " + a);
             System.out.println(read(a,write));
         }
         );
 
+        System.out.println();
+        System.out.println("Удаление по индексу");
+        removeByIndex(users.get(0).getUserPay(), write);
+        for (var entry : write.entrySet()) {
+            User user = read(entry.getKey(), write);
+            if (user != null) {
+                System.out.println("Key: " + entry.getKey());
+                System.out.println(read(entry.getKey(), write));
+            }
+        }
+
+        System.out.println();
+        System.out.println("Вывод объекта,находящегося ниже заданного");
+        System.out.println(move(users.get(1).getUserPay(), write, 0));
+        System.out.println("Вывод объекта,находящегося выше заданного");
+        System.out.println(move(users.get(2).getUserPay(), write, 1));
     }
 }
--- src/Lab_8/v_5/User.java
+++ src/Lab_8/v_5/User.java
@@ -5,20 +5,126 @@ import java.lang.reflect.Field;
 import java.util.Date;
 
 public class User implements Serializable {
+    private boolean deleted = false;
     private String name;
     private String surname;
     private String patronymic;
     private String phone;
     private String area;
     private String address;
-    private long dealNumber;
+    private Long dealNumber;
     private Date dealDate;
-    private long userPay;
+    private Long userPay;
     private Date lastPay;
     private static String field = "name";
     public static String getFieldVal(){
         return field;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getDealNumber() {
+        return dealNumber;
+    }
+
+    public void setDealNumber(Long dealNumber) {
+        this.dealNumber = dealNumber;
+    }
+
+    public Date getDealDate() {
+        return dealDate;
+    }
+
+    public void setDealDate(Date dealDate) {
+        this.dealDate = dealDate;
+    }
+
+    public Long getUserPay() {
+        return userPay;
+    }
+
+    public void setUserPay(Long userPay) {
+        this.userPay = userPay;
+    }
+
+    public Date getLastPay() {
+        return lastPay;
+    }
+
+    public void setLastPay(Date lastPay) {
+        this.lastPay = lastPay;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                " name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", phone='" + phone + '\'' +
+                ", area='" + area + '\'' +
+                ", address='" + address + '\'' +
+                ", dealNumber=" + dealNumber +
+                ", dealDate=" + dealDate +
+                ", userPay=" + userPay +
+                ", lastPay=" + lastPay +
+                '}';
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
     public static void setField(String field1) {
         field = field1;
     }
@@ -42,19 +148,4 @@ public class User implements Serializable {
         this.lastPay = lastPay;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", patronymic='" + patronymic + '\'' +
-                ", phone='" + phone + '\'' +
-                ", area='" + area + '\'' +
-                ", address='" + address + '\'' +
-                ", dealNumber=" + dealNumber +
-                ", dealDate=" + dealDate +
-                ", userPay=" + userPay +
-                ", lastPay=" + lastPay +
-                '}';
-    }
 }
