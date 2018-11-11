c1ec58ea81c0758f6e51dd264328c5884157e96f
DevandScorp <artemda459@gmail.com>
2018-11-11 16:49:38 +0300

    feat: lab_9

---
+++ src/Lab_9/v_5/TestASClient.java
@@ -0,0 +1,42 @@
+package Lab_9.v_5;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class TestASClient implements Runnable{
+    private Socket clientSocket;
+
+    public TestASClient(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        try(BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+            DataOutputStream oos = new DataOutputStream(clientSocket.getOutputStream());
+            DataInputStream ois = new DataInputStream(clientSocket.getInputStream()))
+        {
+            System.out.println("User attended delivery server.");
+            System.out.println(ois.readUTF());
+            System.out.println("Write your dishes numbers and delivery address");
+            String clientCommand = br.readLine();
+            oos.writeUTF(clientCommand);
+            oos.flush();
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) throws InterruptedException, IOException {
+        new Thread(new TestASClient(new Socket("localhost",3345))).start();
+    }
+
+
+}
---
+++ src/Lab_9/v_5/TestAsServer.java
@@ -0,0 +1,25 @@
+package Lab_9.v_5;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TestAsServer {
+
+    public static void main(String[] args) throws InterruptedException {
+        try (ServerSocket server= new ServerSocket(3345)){
+            while(true){
+                Socket client = server.accept();
+                DataOutputStream out = new DataOutputStream(client.getOutputStream());
+                DataInputStream in = new DataInputStream(client.getInputStream());
+                out.writeUTF("Our menu:\n" +
+                                 "\t\t1.Beef steak\n\t\t2.Pancake\n\t\t3.Borsch\n\t\t4.Bouillon\n\t\t5.Open sandwich\n\t\t6.Curd\n\t\t7.Beetroot salad\n\t\t8.Hamburger\n");
+                String entry = in.readUTF();
+                System.out.println("Delivery info: " + entry);
+
+                out.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
